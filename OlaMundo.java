ff5db0e71aa80ace7b2fea8d40a8c5bbd52ec4aa
Yan Miura <82350314+ymiuraa@users.noreply.github.com>
2021-08-18 22:30:55 -0300

    Ex 1 - Tabuada do 10

---
+++ OlaMundo.java
@@ -0,0 +1,16 @@
+package Java;
+
+/**
+ * mat:0050014970 - Yan Miura
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        
+        for (int i = 10; i<=10; i++){
+			for (int j = 0; j<=10; j++){
+				System.out.println (i+" x "+ j + " = " + i*j);
+			}
+		}
+	}
+}
